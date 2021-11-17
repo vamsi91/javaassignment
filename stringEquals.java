b99428d77a09ef017cdeba73138b938fc7fda635
vamsi krishna Reddy <36884184+vamsi91@users.noreply.github.com>
2021-11-17 17:52:42 +0530

    Add files via upload

---
+++ stringEquals.java
@@ -0,0 +1,18 @@
+package javaassignments;
+
+public class stringEquals {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		String s1 = "vamsikrishna";
+		String s2 = "vamsikrishna";
+		String s3 = "VAMSIKRISHNA";
+		String s4 = "pradeep";
+		System.out.println(s1.equals(s2)); 
+		System.out.println(s1.equals(s3));
+		System.out.println(s1.equals(s4));
+
+	}
+
+}
---
+++ subStrings.java
@@ -0,0 +1,17 @@
+package javaassignments;
+
+public class subStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s5="Welcome to java";  
+		System.out.println(s5.substring(0,15));
+		System.out.println(s5.substring(0,7)); 
+		System.out.println(s5.substring(11,15)); 
+		
+		String s6="Welcome to java";  
+	System.out.println(s6.replace('a','e')); 
+
+	}
+
+}
