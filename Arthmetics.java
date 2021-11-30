c4e329baa86cfeb98c54afa57704ac6ec4fdd146
vamsi krishna Reddy <36884184+vamsi91@users.noreply.github.com>
2021-11-30 19:36:00 +0530

    Add files via upload

--- Arthmetics.java
+++ Arthmetics.java
@@ -1,10 +1,51 @@
 package javaTasks;
 
+import java.util.Scanner;
+
 public class Arthmetics {
+	void add() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter First number");
+		int c= scan.nextInt();
+		System.out.println("Enter second number");
+		int d = scan.nextInt();
+		System.out.println("The sum is "+ (c+d));
+	}
+	
+	void sub() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter First number");
+		int c= scan.nextInt();
+		System.out.println("Enter second number");
+		int d = scan.nextInt();
+		System.out.println("The substraction is "+ (c-d));
+	}
+	void mul() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter First number");
+		int c= scan.nextInt();
+		System.out.println("Enter second number");
+		int d = scan.nextInt();
+		System.out.println("The multiplication is "+ (c*d));
+	}
+	void div() {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter First number");
+		int c= scan.nextInt();
+		System.out.println("Enter second number");
+		int d = scan.nextInt();
+		System.out.println("The division is "+ (c/d));
+	}
+
+
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		 Arthmetics  ar = new   Arthmetics();
+		 ar.add();
+		 ar.sub();
+		 ar.mul();
+		 ar.div();
 	}
 
 }
